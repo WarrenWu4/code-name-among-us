2e3c532181f8a5b581da6394fd29791b07b3cf9b
WarrenWu4 <warrenweiwu04@gmail.com>
2022-10-16 11:00:01 -0500

    final commit

--- card.js
+++ card.js
@@ -6,5 +6,7 @@ card.onclick = function discoverCard() {
   let className = document.getElementById('card1-1').className
   if (className != 'discovered') {
     document.getElementById('card1-1').className = 'discovered'
+    document.getElementById('card1-1').getElementsByClassName('background')[0].remove()
+    document.getElementById('card1-1').getElementsByClassName('board-text')[0].remove()
   }
 }
